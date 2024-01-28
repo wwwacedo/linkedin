de76f358efbcbe5f511c771e45fc486e4fef9ed8
Mariana M. Macêdo <eng.marianammacedo@gmail.com>
2024-01-28 08:18:15 -0300

    finalizando post 1

--- post-1/post-1.js
+++ post-1/post-1.js
@@ -1,26 +1,18 @@
-const counter = (number = 0) => {
-    return new Promise((resolve, reject) => {
-        if (typeof number !== 'number' || number < 0) {
-            reject(new Error('Input must be a non-negative integer'));
-            return;
-        }
+const countdown = (number = 0) => {
+    if (typeof number !== 'number' || number < 0) {
+        console.log('Input must be a non-negative integer');
+        return;
+    }
 
-        const countdown = (num) => {
-            if (num > 0) {
-                const timeoutId = setTimeout(() => {
-                    console.log(num);
-                    countdown(num - 1);
-                }, 1000);
-                return () => clearTimeout(timeoutId);
-            } else {
-                resolve('Counter completed!');
-            }
-        };
-
-        countdown(number);
-    });
+    if (number > 0) {
+        setTimeout(() => {
+            console.log(number);
+            countdown(number - 1);
+        }, 1000);
+    } else {
+        console.log('Counter completed!');
+    }
 };
 
-counter(5)
-    .then(message => console.log(message))
-    .catch(error => console.error(error.message));
+countdown(5);
+
