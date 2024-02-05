bd8c79a1e1a7047f3f068167e601a964b71c496d
wwwacedo <eng.marianammacedo@gmail.com>
2024-02-05 15:59:39 -0300

    updating post-4

---
+++ post-4/code.js
@@ -0,0 +1,20 @@
+// Regular function
+// function square(a) {
+// 	return a * a;
+// }
+
+// Function expression
+// const square = function(a) {
+// 	return a * a;
+// }
+
+// Arrow function
+// const square = (a) => {
+// 	return a * a;
+// }
+
+// Arrow function with one line
+const square = a => a * a;
+
+
+
