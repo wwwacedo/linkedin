d489194865ea33297abf9604a9b504bde498f85f
wwwacedo <eng.marianammacedo@gmail.com>
2024-02-23 05:57:10 -0300

    update post-1

--- post-1/post-1.js
+++ post-1/post-1.js
@@ -1,18 +1,17 @@
 const countdown = (number = 0) => {
-    if (typeof number !== 'number' || number < 0) {
-        console.log('Input must be a non-negative integer');
-        return;
-    }
+	if (typeof number !== 'number' || number < 0) {
+		console.log('Input must be a non-negative integer!');
+		return;
+	}
 
-    if (number > 0) {
-        setTimeout(() => {
-            console.log(number);
-            countdown(number - 1);
-        }, 1000);
-    } else {
-        console.log('Counter completed!');
-    }
-};
+	setTimeout(() => {
+		if (number > 0) {
+			console.log(number);
+			return countdown(number - 1);
+		}
+		console.log('Counter completed!');
+	}, 1000);
+
+}
 
 countdown(5);
-
